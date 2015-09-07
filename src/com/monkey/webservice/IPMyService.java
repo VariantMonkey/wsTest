6e79988abd746cc2cc10bf1472b69eb12d0ce7f2
452101765@qq.com <452101765@qq.com>
2015-09-07 20:28:22 +0800

    2

--- src/com/monkey/webservice/IPMyService.java
+++ src/com/monkey/webservice/IPMyService.java
@@ -10,7 +10,7 @@ public class IPMyService {
     public static void main(String[] args) {
         String  address="http://192.168.1.104:8889/ns";
         Endpoint.publish(address,new MyServiceImpl());
-        System.out.println("aaaa");
+        System.out.println("aaaabbb");
     }
 }
 
